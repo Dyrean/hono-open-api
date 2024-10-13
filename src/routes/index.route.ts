b813cfdfd408c70cafe23acdf07dd67578a95eaf
Dyrean <fcanbeyy@gmail.com>
2024-10-14 02:04:44 +0300

    first commit

---
+++ src/routes/index.route.ts
@@ -0,0 +1,29 @@
+import { createRoute, z } from "@hono/zod-openapi";
+import * as HttpStatusCodes from "stoker/http-status-codes";
+import { jsonContent } from "stoker/openapi/helpers";
+
+import { createRouter } from "@/lib/create-app";
+
+const router = createRouter()
+    .openapi(createRoute({
+        method: "get",
+        path: "/",
+        responses: {
+            [HttpStatusCodes.OK]: jsonContent(
+                z.object({
+                    message: z.string(),
+                }).openapi({
+                    example: {
+                        message: "Tasks API",
+                    },
+                }),
+                "Tasks API Index",
+            ),
+        },
+    }), (c) => {
+        return c.json({
+            message: "Tasks API",
+        }, HttpStatusCodes.OK);
+    });
+
+export default router;
